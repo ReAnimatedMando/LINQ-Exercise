f3ec5099c14b3bb5e55df0cf8e455e68d02aa6f7
Aaron Tanguma <the_guma@yahoo.com>
2025-12-25 08:41:53 -0700

    sorted list by length of game name, ascending, wrote results to the console using a foreach loop

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
-            var videoGames = new string[] { "Silent Hill", "Call of Duty", "Elden Ring", "Bioshock", "Little Nightmares" };
+            // Create a list of video games
+            var videoGames = new List<string> { "Silent Hill", "Call of Duty", "Elden Ring", "Bioshock", "Little Nightmares" };
             
+            // Order the list of games by length of the game name
+            var sortByNameLength = videoGames.OrderBy(videoGames => videoGames.Length).ToList();
+            // use a foreach loop to write results to the console
+            sortByNameLength.ForEach(game => Console.WriteLine(game));
         }
     }
 }
--- LINQ/obj/Debug/net9.0/LINQ.AssemblyInfo.cs
+++ LINQ/obj/Debug/net9.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997eadd12c8f8606fe63891c9d2e8ff05f830169")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0d26031cd4641fcbaa95e5c49a39b74db0692b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
