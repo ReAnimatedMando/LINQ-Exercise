9e0d26031cd4641fcbaa95e5c49a39b74db0692b
Aaron Tanguma <the_guma@yahoo.com>
2025-12-25 08:29:06 -0700

    created a list of video games

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new string[] { "Silent Hill", "Call of Duty", "Elden Ring", "Bioshock", "Little Nightmares" };
+            
         }
     }
 }
